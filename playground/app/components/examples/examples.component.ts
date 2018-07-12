09f13e5be6de21b225a82ac329897627170eba42
Raymond <ray@firestitch.com>
2018-07-12 12:30:42 -0400

    added install example

--- playground/app/components/examples/examples.component.ts
+++ playground/app/components/examples/examples.component.ts
@@ -4,4 +4,12 @@ import { Component } from '@angular/core';
   templateUrl: 'examples.component.html'
 })
 export class ExamplesComponent {
+
+  mainModule = `@NgModule({
+      FsMetaModule.forRoot({
+        defaults: [
+          { property: 'og:title', content: 'The Rock' },
+          { property: 'og:test', content: 'The Test' },
+        ]
+      });`;
 }
