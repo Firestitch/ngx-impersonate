ff808950291f10af584469519fb4b3383ae17af0
Raymond Gigliotti <ray@firestitch.com>
2023-10-19 11:25:29 -0400

    added changeDetection

--- playground/app/components/examples/examples.component.ts
+++ playground/app/components/examples/examples.component.ts
@@ -1,19 +1,14 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
 import { environment } from '../../../environments/environment';
 
 
 @Component({
-  templateUrl: 'examples.component.html'
+  templateUrl: 'examples.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ExamplesComponent {
 
   public config = environment;
 
-  mainModule = `@NgModule({
-      FsMetaModule.forRoot({
-        defaults: [
-          { property: 'og:title', content: 'The Rock' },
-          { property: 'og:test', content: 'The Test' },
-        ]
-      });`;
 }
